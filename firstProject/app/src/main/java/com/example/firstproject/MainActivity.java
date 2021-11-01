e54902c14b02ecfb3ce5dd471885c8006eda2bfb
muhammad-farooq-786 <farooqawaisi6310@gmail.com>
2021-11-01 19:31:21 +0500

    new activity and intent added

--- firstProject/app/src/main/java/com/example/firstproject/MainActivity.java
+++ firstProject/app/src/main/java/com/example/firstproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.firstproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +10,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button buttonIntent = findViewById(R.id.calculate);
 
 
 
@@ -17,22 +18,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText qNamaz = findViewById(R.id.qazaNamaz);
+                EditText qRoza = findViewById(R.id.qazaRoza);
+                EditText qWitr = findViewById(R.id.qazaWitr);
+                int namaz = Integer.parseInt(qNamaz.getText().toString());
+                int witr = Integer.parseInt(qWitr.getText().toString());
+                int roza = Integer.parseInt(qRoza.getText().toString());
+                namaz=namaz*140;
+                roza*=140;
+                witr*=140;
+                int total = namaz+roza+witr;
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("result",total);
+                //startActivity(buttonIntent);
+                //well there can be many things
+                //and you should be doing those
+            }
+        });
     }
 
-
-    public void calculate(View view){
-        EditText qNamaz = findViewById(R.id.qazaNamaz);
-        EditText qRoza = findViewById(R.id.qazaRoza);
-        EditText qWitr = findViewById(R.id.qazaWitr);
-        int namaz = Integer.parseInt(qNamaz.getText().toString());
-        int witr = Integer.parseInt(qWitr.getText().toString());
-        int roza = Integer.parseInt(qRoza.getText().toString());
-        namaz=namaz*140;
-        roza*=140;
-        witr*=140;
-        int total = namaz+roza+witr;
-
-        TextView result = findViewById(R.id.result);
-        result.setText(String.valueOf(total));
-    }
 }
---
+++ firstProject/app/src/main/java/com/example/firstproject/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.firstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        intent.getStringExtra("result");
+        EditText editText = findViewById(R.id.total);
+        editText.setText((intent.getData().toString()));
+    }
+}
